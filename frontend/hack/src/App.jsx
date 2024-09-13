61944ae5fec0a78bc64c358bf892e3c5d5717e47
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-09-14 03:00:59 +0530

    ok

--- frontend/hack/src/App.jsx
+++ frontend/hack/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import LoginPage from "./Pages/LoginPage/LoginPage"
+import Home from "./Pages/Home/Home"
 export default function app(){
   return(
     <>
-    <LoginPage />
+    {/* <LoginPage /> */}
+    <Home/>
     </>
   )
 }
---
+++ frontend/hack/src/Pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "./Home.css"
+import BG from "../../assets/BG/football pitch.jpg"
+import BG2 from "../../assets/BG/footballmain.jpeg"
+import LOGO from "../../components/3D_Logo/3DLogo"
+import BG3 from "../../assets/BG/FG.png"
+
+export default function Home(){
+    return(
+        <div className="home">
+            {/* <img src={BG3} /> */}
+            <div className="overlay">
+                <div className="home-box">
+                <div className="home-box-1">
+                    <div className="home-box-text">
+                    <h1>AthleX AI</h1>
+                    <h2>FOR COACHES,</h2>
+                    <h2>BY <span>AI</span></h2>
+                    </div>
+                </div>
+                <div className="home-box-2">
+                    <LOGO/>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
